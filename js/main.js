384f715299eab48d3683ef9b7f9565b431a30e11
Michelle <vand0432@algonquinlive.com>
2016-03-28 12:43:36 -0400

    Diamond slides.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 var $body = $('body');
+
 var $btnShowHide = $('.btn-show-hide');
 var $box = $('.box');
 
+var $btnMove = $('.btn-move');
+var $diamond = $('.diamond');
+
+
 $btnShowHide.on('click', function () {
   $box.toggleClass('showbox');
+});
+
+$btnMove.on('click', function () {
+  $diamond.toggleClass('btn-slide')
 });
