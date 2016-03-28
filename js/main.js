515a214d41c3668b26e1d6c695e89dd66243a256
Michelle <vand0432@algonquinlive.com>
2016-03-28 18:16:58 -0400

    Collapsed the yellow box.

--- js/main.js
+++ js/main.js
@@ -6,11 +6,19 @@ var $box = $('.box');
 var $btnMove = $('.btn-move');
 var $diamond = $('.diamond');
 
+var $btnColEx = $('.btn-collapse-expand');
+var $panel = $('.panel');
 
 $btnShowHide.on('click', function () {
   $box.toggleClass('showbox');
 });
 
 $btnMove.on('click', function () {
-  $diamond.toggleClass('btn-slide')
+  $diamond.toggleClass('btn-slide');
+});
+
+// do the collapse thingy test.
+$btnColEx.on('click', function () {
+  $panel.toggleClass('collapse-panel');
+
 });
